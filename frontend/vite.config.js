04d8fd4f5b3ac7c7e74b2c3aadfcaf817f83781f
iasiddiqui <115731094+iasiddiqui@users.noreply.github.com>
2025-05-01 22:26:02 +0530

    some chnages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Load environment variables based on the current mode (development or production)
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
-    base: mode === 'production' ? '/blog-cms/' : '/',
+    base: '/', 
     define: {
       'process.env': {
         VITE_API_URL: JSON.stringify(env.VITE_API_URL),
