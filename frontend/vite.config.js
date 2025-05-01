788320a922a41a5f0eb527eb5a4768d42bc2fcf0
iasiddiqui <115731094+iasiddiqui@users.noreply.github.com>
2025-05-01 16:54:23 +0530

    some chnages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
+  base: process.env.NODE_ENV === 'production' ? '/your-project-name/' : '/',
+  define: {
+    'process.env': {
+      VITE_API_URL: process.env.VITE_API_URL,
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,  // Set this according to your app size
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
